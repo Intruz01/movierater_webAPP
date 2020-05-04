0256d01b5617d913c65855e470127be8bf21d45a
Serhiy <serhiy.chubyk@hotmail.com>
2020-05-05 01:53:08 +0200

    making connection between django and angular

--- django/movierater/movieraterApp/serializers.py
+++ django/movierater/movieraterApp/serializers.py
@@ -5,4 +5,4 @@ from .models import Movie
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ['id', 'title', 'desription', 'year', 'imdb_rating']
+        fields = ['id', 'title', 'description', 'year', 'imdb_rating']
--- django/movierater/settings.py
+++ django/movierater/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:4200",
+]
+
 
 # Application definition
 
@@ -37,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'movierater.movieraterApp'
+    'movierater.movieraterApp',
+    'corsheaders'
 
 ]
 
@@ -49,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'movierater.urls'
